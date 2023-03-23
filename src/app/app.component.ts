a0b7d5b6185ca532e8639f7e7d0b2011ea447c71
chai <chaithra.amin98@gmail.com>
2023-03-23 14:54:14 +0530

    header website with mobile size nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website-header-design';
+  ngOnInit(){
+      var navList:any=document.getElementById("nav-list");
+      navList.style.maxHeight="0px";
+  }
+  toggleMenu(){
+    var navList:any=document.getElementById("nav-list");
+    if( navList.style.maxHeight=="0px"){
+      navList.style.maxHeight="300px";
+    }
+    else{
+      navList.style.maxHeight="0px";
+    }
+  }
 }
